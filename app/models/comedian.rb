115677538918f97a84f67799f41e2e12a5521641
Eli Sachs <easachs13@gmail.com>
2022-07-22 09:57:03 -0600

    MVC refactor

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -5,10 +5,20 @@ class Comedian < ApplicationRecord
   validates_presence_of :age
 
   def self.average_age
-    "#{average(:age).round(2)} Years"
+    average(:age)
+    # "#{average(:age).round(2)} Years"
   end
 
   def average_special_runtime
     specials.average(:runtime)
+  end
+
+  def total_specials
+    specials.count
+  end
+
+  def longest_special
+    # specials.order(runtime: :desc).limit(1)
+    specials.maximum(:runtime)
   end
 end
